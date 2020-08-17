291fa1ac579777850e5749c88dfe14d85c763c47
ericmbouwe <ericmbouwe@yahoo.fr>
2020-08-17 15:26:01 +0100

    added function add segments to snake

--- scripts/food.js
+++ scripts/food.js
@@ -1,7 +1,13 @@
-let food = { x: 0, y: 0 }
+import { onSnake, expandSnake } from "./snake.js";
+// set our food postion in the grid (x -> column, y -> row)
+let food = { x: 10, y: 2 }
+const EXPANSION_RATE = 1
 
 export function update() {
-
+  if (onSnake(food)) {
+    expandSnake(EXPANSION_RATE)
+    food = { x: 20, y: 10 }
+  }
 }
 
 export function draw(gameBoard) {
--- scripts/snake.js
+++ scripts/snake.js
@@ -1,14 +1,17 @@
 import { getInputDirection } from "./inputs.js"
 
 export const SNAKE_SPEED = 5
+let newSegments = 0
 
 // set our snake postion in the grid (x -> row, y -> column)
 const snakeBody = [{ x: 11, y: 11 }]
 
 export function update() {
   // the logic here is to shift the head and make each segment(but not the tail) below the head to take the previous position of his following
-  // snakeBody[i + 1] is the segment before the last one, it take the place of the last. the process is repeated until i = 0
 
+  addSegments()
+  
+  // snakeBody[i + 1] is the segment before the last one, it take the place of the last. the process is repeated until i = 0
   const inputDirection = getInputDirection()
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] }
@@ -28,4 +31,25 @@ export function draw(gameBoard) {
     snakeElement.className = 'snake'
     gameBoard.appendChild(snakeElement)
   })
+}
+
+export function expandSnake(amount) {
+  newSegments += amount
+}
+
+// If any segment of the snake equals the position passed as argument, this return true
+export function onSnake(position) {
+  return snakeBody.some(segment => {
+    return equalPositions(segment, position)
+  })
+}
+
+function equalPositions(pos1, pos2) {
+  return pos1.x === pos2.x && pos1.y === pos2.y
+}
+
+function addSegments() {
+  for (let i = 0; i < newSegments; i++) {
+    snakeBody.push({ ...snakeBody[snakeBody.length - 1] })
+  }
 }
